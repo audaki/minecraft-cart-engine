00f412654a6939701c47976061a35c4b8e7f8c4a
Kira M. Backes <kira.backes@nrwsoft.de>
2026-04-26 15:31:03 +0200

    Update to modern standards, add code formatting settings

--- src/main/java/audaki/cart_engine/AceGameRules.java
+++ src/main/java/audaki/cart_engine/AceGameRules.java
@@ -8,18 +8,19 @@ import org.jetbrains.annotations.NotNull;
 public class AceGameRules {
 
     // All speeds in blocks per second
-    private static final String ID_MAX_SPEED_PLAYER_RIDER = "ace:speed_player";
-    private static final String ID_MAX_SPEED_OTHER_RIDER  = "ace:speed_other";
-    private static final String ID_MAX_SPEED_EMPTY_RIDER  = "ace:speed_empty";
+    private static final String NAMESPACE = "ace";
+    private static final String ID_MAX_SPEED_PLAYER_RIDER = "speed_player";
+    private static final String ID_MAX_SPEED_OTHER_RIDER  = "speed_other";
+    private static final String ID_MAX_SPEED_EMPTY_RIDER  = "speed_empty";
 
     public static final GameRule<@NotNull Integer> MINECART_MAX_SPEED_PLAYER_RIDER =
-            GameRuleBuilder.forInteger(20).buildAndRegister(Identifier.parse(ID_MAX_SPEED_PLAYER_RIDER));
+            GameRuleBuilder.forInteger(20).buildAndRegister(Identifier.fromNamespaceAndPath(NAMESPACE, ID_MAX_SPEED_PLAYER_RIDER));
 
     public static final GameRule<@NotNull Integer> MINECART_MAX_SPEED_OTHER_RIDER =
-            GameRuleBuilder.forInteger(0).buildAndRegister(Identifier.parse(ID_MAX_SPEED_OTHER_RIDER));
+            GameRuleBuilder.forInteger(0).buildAndRegister(Identifier.fromNamespaceAndPath(NAMESPACE, ID_MAX_SPEED_OTHER_RIDER));
 
     public static final GameRule<@NotNull Integer> MINECART_MAX_SPEED_EMPTY_RIDER =
-            GameRuleBuilder.forInteger(0).buildAndRegister(Identifier.parse(ID_MAX_SPEED_EMPTY_RIDER));
+            GameRuleBuilder.forInteger(0).buildAndRegister(Identifier.fromNamespaceAndPath(NAMESPACE, ID_MAX_SPEED_EMPTY_RIDER));
 
     public static void register() {}
 }
